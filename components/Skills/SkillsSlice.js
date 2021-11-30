b0f600be45767e4c6b635d5edda5982425c11678
Guilherme Below <guilherme.below@hotmail.com>
2021-11-30 09:17:09 -0300

    skills ready with redux

--- components/Skills/SkillsSlice.js
+++ components/Skills/SkillsSlice.js
@@ -1,51 +1,57 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { selectStatsModifier } from '../Stats/StatsSlice';
+import { useSelector } from 'react-redux';
 
 export const slice = createSlice({
   name: 'skills',
   initialState: {
-    appraise: { ability: 'int', armorPen:false, requiredTraining:false},
-    balance: { ability: 'dex', armorPen:true, requiredTraining:false},
-    bluff: { ability: 'cha', armorPen:false, requiredTraining:false},
-    climb: { ability: 'str', armorPen:true, requiredTraining:false},
-    concentration: { ability: 'con', armorPen:false, requiredTraining:false},
-    decipher: { ability: 'int', armorPen:false, requiredTraining:true},
-    diplomacy: { ability: 'cha', armorPen:false, requiredTraining:false},
-    disable: { ability: 'dex', armorPen:false, requiredTraining:true},
-    disguise: { ability: 'cha', armorPen:false, requiredTraining:false},
-    escape: { ability: 'dex', armorPen:true, requiredTraining:false},
-    forgery: { ability: 'int', armorPen:false, requiredTraining:false},
-    gather: { ability: 'cha', armorPen:false, requiredTraining:false},
-    handle_animal: { ability: 'cha', armorPen:false, requiredTraining:true},
-    heal: { ability: 'wis', armorPen:false, requiredTraining:false},
-    hide: { ability: 'dex', armorPen:true, requiredTraining:false},
-    intimidate: { ability: 'cha', armorPen:false, requiredTraining:false},
-    jump: { ability: 'str', armorPen:true, requiredTraining:false},
-    listen: { ability: 'wis', armorPen:false, requiredTraining:false},
-    move_silently: { ability: 'dex', armorPen:true, requiredTraining:false},
-    open_lock: { ability: 'dex', armorPen:false, requiredTraining:true},
-    ride: { ability: 'dex', armorPen:false, requiredTraining:false},
-    search: { ability: 'int', armorPen:false, requiredTraining:false},
-    sense_motive: { ability: 'wis', armorPen:false, requiredTraining:false},
-    sleight_of_hand: { ability: 'dex', armorPen:true, requiredTraining:true},
-    spellcraft: { ability:'int', armorPen:false, requiredTraining:true},
-    spot: { ability:'wis', armorPen:false, requiredTraining:false},
-    survival: { ability: 'wis', armorPen:false, requiredTraining:false},
-    swim: { ability: 'str', armorPen:true, requiredTraining:false},
-    tumble: { ability: 'dex', armorPen:true, requiredTraining:true},
-    use_magic_device: { ability: 'cha', armorPen:false, requiredTraining:false},
-    use_rope: { ability: 'dex', armorPen:false, requiredTraining:false},
+    appraise: { ability: 'INT', armorPen:false, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    balance: { ability: 'DEX', armorPen:true, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    bluff: { ability: 'CHA', armorPen:false, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    climb: { ability: 'STR', armorPen:true, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    concentration: { ability: 'CON', armorPen:false, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    decipher: { ability: 'INT', armorPen:false, requiredTraining:true, ranks:0, miscMod:0, isClassSkill:false},
+    diplomacy: { ability: 'CHA', armorPen:false, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    disable: { ability: 'DEX', armorPen:false, requiredTraining:true, ranks:0, miscMod:0, isClassSkill:false},
+    disguise: { ability: 'CHA', armorPen:false, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    escape: { ability: 'DEX', armorPen:true, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    forgery: { ability: 'INT', armorPen:false, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    gather: { ability: 'CHA', armorPen:false, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    handle_animal: { ability: 'CHA', armorPen:false, requiredTraining:true, ranks:0, miscMod:0, isClassSkill:false},
+    heal: { ability: 'WIS', armorPen:false, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    hide: { ability: 'DEX', armorPen:true, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    intimidate: { ability: 'CHA', armorPen:false, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    jump: { ability: 'STR', armorPen:true, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    listen: { ability: 'WIS', armorPen:false, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    move_silently: { ability: 'DEX', armorPen:true, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    open_lock: { ability: 'DEX', armorPen:false, requiredTraining:true, ranks:0, miscMod:0, isClassSkill:false},
+    ride: { ability: 'DEX', armorPen:false, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    search: { ability: 'INT', armorPen:false, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    sense_motive: { ability: 'WIS', armorPen:false, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    sleight_of_hand: { ability: 'DEX', armorPen:true, requiredTraining:true, ranks:0, miscMod:0, isClassSkill:false},
+    spellcraft: { ability:'INT', armorPen:false, requiredTraining:true, ranks:0, miscMod:0, isClassSkill:false},
+    spot: { ability:'WIS', armorPen:false, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    survival: { ability: 'WIS', armorPen:false, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    swim: { ability: 'STR', armorPen:true, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    tumble: { ability: 'DEX', armorPen:true, requiredTraining:true, ranks:0, miscMod:0, isClassSkill:false},
+    use_magic_device: { ability: 'CHA', armorPen:false, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
+    use_rope: { ability: 'DEX', armorPen:false, requiredTraining:false, ranks:0, miscMod:0, isClassSkill:false},
   },
   reducers: {
     changeSkillItemValue: (state, action) => {
       state[action.payload.itemName][action.payload.valueName] = parseInt(action.payload.value) ? parseInt(action.payload.value) : 0 ;
     },    
+    createSkillItem: (state, action) => {
+      state[action.payload.itemName] = {...action.payload.value, ranks:0, miscMod:0, isClassSkill:false}
+    },    
   },
 });
 
-export const { changeSkillItemValue } = slice.actions;
+export const { changeSkillItemValue, createSkillItem } = slice.actions;
 
 export const selectSkillItem = (itemName) => state => state.skills[itemName];
 export const selectAllSkills = state => state.skills;
+export const selectSkillTotal = skillName => state => state.skills[skillName].ranks +  state.skills[skillName].miscMod + useSelector(selectStatsModifier(state.skills[skillName].ability))
 
 
 export default slice.reducer;
--- components/Skills/SkillsTable.js
+++ components/Skills/SkillsTable.js
@@ -1,46 +1,49 @@
-import React from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View, CheckBox } from 'react-native';
 import { Cell } from '../CellComponents';
 import { UnderlinedText, UnderlinedTextInput, TitleText } from '../TextComponents';
 import SelectDropdown from 'react-native-select-dropdown'
 import { useDispatch, useSelector } from 'react-redux';
-import { changeSkillItemValue, selectSkillsItem, selectAllSkills } from './SkillsSlice';
+import { changeSkillItemValue, selectSkillsItem, selectAllSkills, selectSkillTotal, createSkillItem } from './SkillsSlice';
+import {selectStatsModifier} from '../Stats/StatsSlice';
 
 export function SkillsTable ({}){
 
   const dispatch = useDispatch()  
   const setter = (itemName, valueName)=> (e)=>dispatch(changeSkillItemValue({itemName:itemName, valueName:valueName ,value:e}))
+  const createItem = (name, skill)=>(e)=>dispatch(createSkillItem({itemName:name, value:skill}))
   const itemSelector = itemName => useSelector(selectSkillsItem(itemName))
   const allSkillsSelector = () => useSelector(selectAllSkills)
   const modifierSelector = itemName => useSelector(selectStatsModifier(itemName))
+  const skillTotalSelecter = itemName => useSelector(selectSkillTotal(itemName))
 
   const allSkills = allSkillsSelector()
-
-  const SkillItem = ({item}) => {
+  
+  const SkillItem = ({item, name}) => {
     return(
       <View key={item.name} style={styles.skillItem}>
-        <View style={{flex:0.5}}>          
+        <View style={{flex:0.5}}>
         </View>
         <View style={{flex:2}}>
-          <Text>{item.name}</Text>
+          <Text>{name}</Text>
         </View>
         <View style={{flex:1}}>
           <Text style={{textAlign:'center',}}>{item.ability}</Text>
         </View>
         <View style={{flex:1}}>
-          <Cell />
+          <Cell content={skillTotalSelecter(name)}/>
         </View>
         <View style={{flex:1, flexDirection:'row', alignItems:'center', }}>
           <Text>=</Text>
-          <UnderlinedText content={'0'}/>
+          <UnderlinedText content={modifierSelector(item.ability)} />
         </View>
         <View style={{flex:1, flexDirection:'row', alignItems:'center', }}>
           <Text>+</Text>
-          <UnderlinedTextInput content={'0'}/>
+          <UnderlinedTextInput content={item.ranks} setContent={setter(name, 'ranks')}/>
         </View>
         <View style={{flex:1, flexDirection:'row', alignItems:'center', }}>
           <Text>+</Text>
-          <UnderlinedTextInput content={'0'}/>
+          <UnderlinedTextInput content={item.miscMod} setContent={setter(name, 'miscMod')}/>
         </View>
         
       </View>
@@ -86,12 +89,45 @@ export function SkillsTable ({}){
     )
   }
 
+  const AddSkill = () => {
+
+    const [skill, setSkill] = useState({ability: 'WIS', armorPen:false, requiredTraining:false})
+    const [name, setName] = useState('')
+  
+    return(
+      <View style={{flexDirection:'row', justifyContent:'space-around', alignItems:'center', height:50, marginVertical:10, borderWidth:1, paddingVertical:5}}>
+        <TextInput style={{borderWidth:1, width:'30%'}} placeholder={'skill name'} onChangeText={(e)=>setName(e)}>{name}</TextInput>
+        <SelectDropdown 
+          buttonStyle={{width:'15%', borderWidth:1, height:'100%'}} 
+          buttonTextStyle={{fontSize:12}} data={['STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA']} 
+          defaultButtonText={'Select Ability'} 
+          onSelect={(e)=>{setSkill({...skill, ability:e})}}
+        />
+        <View className={{paddingVertical:5}}>
+          <Text style={{fontSize:8}} >requires {'\n'} training</Text>
+          <CheckBox
+              value={skill.requiredTraining}
+              onValueChange={(v)=>{setSkill({...skill, requiredTraining:v})}}            
+            />
+        </View>
+        <View className={{paddingVertical:5}}>
+          <Text style={{fontSize:8}}>armor {'\n'} penalty</Text>
+          <CheckBox
+              value={skill.armorPen}
+              onValueChange={(v)=>{setSkill({...skill, armorPen:v})}}            
+            />
+        </View>
+        <Button title={'add skill'}  onPress={createItem(name, skill)}/>
+      </View>
+    )
+  }
+
   return(
     <View style={styles.skillsTable}>
       <SkillsHeader />
       {
         Object.entries(allSkills).map(item => (
-          <SkillItem key={item[0]} item={item[1]} />
+          <SkillItem key={item[0]} item={item[1]} name={item[0]} />
         ))
       }
       <AddSkill />
@@ -99,15 +135,7 @@ export function SkillsTable ({}){
   )
 }
 
-const AddSkill = () => {
-  return(
-    <View style={{flex:1, flexDirection:'row', justifyContent:'space-around', alignItems:'center', marginVertical:10, borderWidth:1, paddingVertical:5}}>
-      <TextInput style={{borderWidth:1, width:'30%'}} placeholder={'skill name'} />
-      <SelectDropdown buttonStyle={{width:'30%', borderWidth:1, height:'100%'}} buttonTextStyle={{fontSize:12}} data={['STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA']} defaultButtonText={'Select Ability'} />
-      <Button title={'add skill'}  />
-    </View>
-  )
-}
+
 
 const styles = StyleSheet.create({
   skillsTable:{
@@ -129,37 +157,3 @@ const styles = StyleSheet.create({
     padding:5,
   }
 })
-
-const skillList = [
-  {name:'appraise', ability: 'int', armorPen:false, requiredTraining:false},
-  {name:'balance', ability: 'dex', armorPen:true, requiredTraining:false},
-  {name:'bluff', ability: 'cha', armorPen:false, requiredTraining:false},
-  {name:'climb', ability: 'str', armorPen:true, requiredTraining:false},
-  {name:'concentration', ability: 'con', armorPen:false, requiredTraining:false},
-  {name:'decipher script', ability: 'int', armorPen:false, requiredTraining:true},
-  {name:'diplomacy', ability: 'cha', armorPen:false, requiredTraining:false},
-  {name:'disable device', ability: 'dex', armorPen:false, requiredTraining:true},
-  {name:'disguise', ability: 'cha', armorPen:false, requiredTraining:false},
-  {name:'escape artist', ability: 'dex', armorPen:true, requiredTraining:false},
-  {name:'forgery', ability: 'int', armorPen:false, requiredTraining:false},
-  {name:'gather information', ability: 'cha', armorPen:false, requiredTraining:false},
-  {name:'handle animal', ability: 'cha', armorPen:false, requiredTraining:true},
-  {name:'heal', ability: 'wis', armorPen:false, requiredTraining:false},
-  {name:'hide', ability: 'dex', armorPen:true, requiredTraining:false},
-  {name:'intimidate', ability: 'cha', armorPen:false, requiredTraining:false},
-  {name:'jump', ability: 'str', armorPen:true, requiredTraining:false},
-  {name:'listen', ability: 'wis', armorPen:false, requiredTraining:false},
-  {name:'move silently', ability: 'dex', armorPen:true, requiredTraining:false},
-  {name:'open lock', ability: 'dex', armorPen:false, requiredTraining:true},
-  {name:'ride', ability: 'dex', armorPen:false, requiredTraining:false},
-  {name:'search', ability: 'int', armorPen:false, requiredTraining:false},
-  {name:'sense motive', ability: 'wis', armorPen:false, requiredTraining:false},
-  {name:'sleight of hand', ability: 'dex', armorPen:true, requiredTraining:true},
-  {name:'spellcraft', ability: 'int', armorPen:false, requiredTraining:true},
-  {name:'spot', ability: 'wis', armorPen:false, requiredTraining:false},
-  {name:'survival', ability: 'wis', armorPen:false, requiredTraining:false},
-  {name:'swim', ability: 'str', armorPen:true, requiredTraining:false},
-  {name:'tumble', ability: 'dex', armorPen:true, requiredTraining:true},
-  {name:'use magic device', ability: 'cha', armorPen:false, requiredTraining:false},
-  {name:'use rope', ability: 'dex', armorPen:false, requiredTraining:false},
-]
